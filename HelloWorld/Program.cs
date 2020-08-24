9c497283e1bddfe1d04b4bbdd9497b9b8a3b5886
S. Santos Pimenta <s.santos.pimenta@avanade.com>
2020-08-24 12:37:40 -0300

    código para hacer que consola solicite mi nombre y aparezca la fecha

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
